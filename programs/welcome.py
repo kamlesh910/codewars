233e99eea39260290ea353250d5cb664e080a458
kamalesh <kamlesh910@gmail.com>
2019-04-04 19:36:15 +0200

    program1

---
+++ programs/welcome.py
@@ -0,0 +1,13 @@
+def greet(language):
+    rs=''
+    d = {'english': 'Welcome','czech': 'Vitejte','danish': 'Velkomst','dutch': 'Welkom','estonian': 'Tere tulemast','finnish': 'Tervetuloa','flemish': 'Welgekomen','french': 'Bienvenue','german': 'Willkommen','irish': 'Failte','italian': 'Benvenuto','latvian': 'Gaidits','lithuanian': 'Laukiamas','polish': 'Witamy','spanish': 'Bienvenido','swedish': 'Valkommen','welsh': 'Croeso'}
+    for a, b in d.items():
+        if language == a:
+            rs=b
+    if rs == '':
+        rs='Welcome'
+    print(rs)
+    return rs
+        
+        
+ 
