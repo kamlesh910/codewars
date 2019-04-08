89e6d7847d059eeaca1ebcfa5451df66400d7aab
Kanchan <kmahajan331@gmail.com>
2019-04-08 23:57:21 +0530

    asgn1

---
+++ programs/Learning Java/twenty.java
@@ -0,0 +1,13 @@
+/*    Write for loop in java to print 20 to 200 */
+
+class twenty {
+    public static void main(String args[])
+    {
+        int x = 20; 
+        while(x<=200) 
+        { 
+            System.out.println("Value of x:" + x); 
+            x++; 
+        }
+   }
+}
---
+++ programs/Learning Java/twentyfor.java
@@ -0,0 +1,9 @@
+/*    Write for loop in java to print 20 to 200 */
+
+class twentyfor {
+    public static void main(String args[])
+    {
+        for (int x = 20; x <= 200 ; x++) 
+            System.out.println("Value of x:" + x); 
+    } 
+}
