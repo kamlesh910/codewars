07ff2d7b1385444d92ba8148417354b464de5a10
Kanchan <kmahajan331@gmail.com>
2019-04-08 23:59:27 +0530

    Update twentyfor.java

--- programs/Learning Java/twentyfor.java
+++ programs/Learning Java/twentyfor.java
@@ -3,7 +3,7 @@
 class twentyfor {
     public static void main(String args[])
     {
-        for (int x = 20; x <= 200 ; x++) 
+        for (int x = 20; x <= 200; x++) 
             System.out.println("Value of x:" + x); 
     } 
 }
