157faa4efb1e12e5efe8dd1f54f835e6535f204b
kamlesh910 <kamlesh910@gmail.com>
2019-04-09 10:21:45 +0200

    Update Whatsupnext.py

--- programs/codewars/Whatsupnext.py
+++ programs/codewars/Whatsupnext.py
@@ -1,11 +1,11 @@
 def next_item(xs, item):
     if item not in xs:
         return None
-        for item in xs:        
-            elif(xs.index(item)==len(xs)-1):
-                print("01 index {0}".format(xs.index(item)))  
-                return None
-            else:
-                index=xs.index(item)
-                print("02 index {0}".format(xs[index+1]))
+    for item in xs:        
+        elif(xs.index(item)==len(xs)-1):
+            print("01 index {0}".format(xs.index(item)))  
+            return None
+        else:
+            index=xs.index(item)
+            print("02 index {0}".format(xs[index+1]))
     return xs[index+1]
