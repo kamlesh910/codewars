0f09ea3b213b61a549cc39dad487a38a67d53a44
Kamalesh Gulab Suryawanshi <kamlesh910@gmail.com>
2019-04-10 14:24:25 +0200

    Update CountOddNumber.py

--- programs/codewars/CountOddNumber.py
+++ programs/codewars/CountOddNumber.py
@@ -1,2 +1,11 @@
 def odd_count(n): 
     return len([x for x in range(1, n) if x%2!=0])
+
+
+def odd_count(n): 
+    li=[]
+    for x in range(1, n):
+        if x%2!=0:
+            li.append(x)
+    return len(li)        
+    
