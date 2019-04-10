736f8a6bfae5bab67fab6603bfaf659c2981a4c7
Kamalesh Gulab Suryawanshi <kamlesh910@gmail.com>
2019-04-10 13:22:30 +0200

    Create ForUFCFans.py

---
+++ programs/codewars/ForUFCFans.py
@@ -0,0 +1,7 @@
+def quote(fighter):
+    ret=''
+    if fighter.lower() == 'george saint pierre':
+        ret="I am not impressed by your performance."
+    elif fighter.lower() == 'conor mcgregor':
+        ret="I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    return ret
