dbf801ce57f3a2bdb1d4810f337c79f5addf014b
Kamalesh Gulab Suryawanshi <kamlesh910@gmail.com>
2021-01-26 16:30:35 +0100

    update

--- programs/Learning Java/basics/twentydo.java
+++ programs/Learning Java/basics/twentydo.java
@@ -1,12 +1,6 @@
 class twentydo {
     public static void main(String args[])
     {
-        int x = 20; 
-        do
-        { 
-            System.out.println("Value of x:" + x); 
-            x++; 
-        } 
-        while (x <=201);
+      
    }
 }
